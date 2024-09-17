7ec467d98fed3f446c2f19444aa46ed37d36aea4
stewpidasle <stewartpidasle@gmail.com>
2024-09-17 15:33:58 -0400

    added tailwindcss to client

---
+++ client/src/containers/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div  className='w-screen h-screen flex items-center justify-center text-blu-600 font-semibold'>App</div>
+  )
+}
+
+export default App;
